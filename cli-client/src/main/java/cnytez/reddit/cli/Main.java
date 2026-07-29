3e4b68706b0604eaed0a8d16c55694286abe3699
Stefan <stefan.cirlomaneanu@s.unibuc.ro>
2026-07-29 10:35:22 +0300

    Add CLI console input and output

--- cli-client/src/main/java/cnytez/reddit/cli/Main.java
+++ cli-client/src/main/java/cnytez/reddit/cli/Main.java
@@ -1,8 +1,17 @@
 package cnytez.reddit.cli;
 
+import cnytez.reddit.cli.ui.ConsolePrinter;
+import cnytez.reddit.cli.ui.ConsoleReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Reddit CLI started");
+        ConsoleReader reader = new ConsoleReader();
+        ConsolePrinter printer = new ConsolePrinter();
+
+        printer.print("Cum te cheama?");
+        String name = reader.readLine();
+
+        printer.println("Salut, " + name + "!");
     }
 }
---
+++ cli-client/src/main/java/cnytez/reddit/cli/ui/ConsolePrinter.java
@@ -0,0 +1,12 @@
+package cnytez.reddit.cli.ui;
+
+public class ConsolePrinter {
+
+    public void print(String message) {
+        System.out.print(message);
+    }
+
+    public void println(String message) {
+        System.out.println(message);
+    }
+}
