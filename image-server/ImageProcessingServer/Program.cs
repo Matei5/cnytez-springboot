61261b29a4de78fbee8388c801207509e797bb8c
denysdobrynin07 <denisdobrynin07@gmail.com>
2026-08-05 14:35:00 +0300

    adapted image processing server to EC2 deployment, added extra filters

--- image-server/ImageProcessingServer/Program.cs
+++ image-server/ImageProcessingServer/Program.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using System.Net;
 using System.Runtime;
 using ImageMagick;
+using Amazon;
+using Amazon.S3.Model;
+using Amazon.Runtime.Internal.Auth;
+using Amazon.S3;
 
 namespace ImageProcessingServer
 {
     public class Program
     {
+        private const string bucketName = "cnytez-image-server-s3";
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUCentral1;
         public static void Main(string[] args)
         {
             var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png", ".webp", ".bmp" };
@@ -22,7 +28,7 @@ namespace ImageProcessingServer
 
             app.MapGet("/", () => "Image processing server is available");
 
-            app.MapPost("/", async (IFormFile file) =>
+            app.MapPost("/{filter?}", async (IFormFile file, IAmazonS3 s3Client, string? filter = null) =>
             {
                 if (file == null || file.Length == 0)
                     return Results.BadRequest("No image sent");
@@ -34,9 +40,23 @@ namespace ImageProcessingServer
 
                 try
                 {
-                    var output = await FilterImageAsync(file);
+                    var output = await FilterImageAsync(file, filter);
 
-                    return Results.File(output, "image/jpeg");
+                    string s3Key = $"processed-images/{Guid.NewGuid()}.jpeg";
+
+                    var putRequest = new PutObjectRequest()
+                    {
+                        BucketName = bucketName,
+                        Key = s3Key,
+                        InputStream = output,
+                        ContentType = "image/jpeg"
+                    };
+
+                    await s3Client.PutObjectAsync(putRequest);
+
+                    string fileUrl = $"http://{bucketName}.s3.{bucketRegion.SystemName}://{s3Key}";
+
+                    return Results.Ok(fileUrl);
                 } 
                 catch (MagickMissingDelegateErrorException)
                 {
@@ -45,6 +65,10 @@ namespace ImageProcessingServer
                 catch (MagickCorruptImageErrorException)
                 {
                     return Results.BadRequest("Image contents corrupt or invalid");
+                }
+                catch (AmazonS3Exception e)
+                {
+                    return Results.Problem(title: "AWS S3 Storage error", detail: e.Message, statusCode: 500);
                 }
                 catch (Exception e)
                 {
@@ -55,12 +79,38 @@ namespace ImageProcessingServer
             app.Run();
         }
 
-        static async Task<MemoryStream> FilterImageAsync(IFormFile file)
+        static async Task<MemoryStream> FilterImageAsync(IFormFile file, string filter)
         {
             await using var input = file.OpenReadStream();
             using var image = new MagickImage(input);
 
-            image.ColorSpace = ColorSpace.Gray;
+            switch(filter)
+            {
+                case "greyscale":
+                    image.ColorSpace = ColorSpace.Gray;
+                    break;
+
+                case "sepia":
+                    image.SepiaTone();
+                    break;
+
+                case "inverted":
+                    image.Negate();
+                    break;
+
+                case "blur":
+                    image.Blur(0, 3);
+                    break;
+
+                case "pixelated":
+                    image.FilterType = FilterType.Point;
+                    image.Resize(new Percentage(20));
+                    image.Resize(new Percentage(500));
+                    break;
+
+                default:
+                    break;
+            }
 
             var output = new MemoryStream();
             await image.WriteAsync(output, MagickFormat.Jpeg);
