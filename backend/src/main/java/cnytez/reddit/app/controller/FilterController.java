8b781ebbd6350941d3a4954c368d2856fae8cdb5
Andrei Preda <andreipreda998@gmail.com>
2026-08-06 13:47:34 +0300

    Fix typo for FilterController request mapping

--- backend/src/main/java/cnytez/reddit/app/controller/FilterController.java
+++ backend/src/main/java/cnytez/reddit/app/controller/FilterController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/filterId")
+@RequestMapping("/filters")
 @RequiredArgsConstructor
 public class FilterController {
     private final FilterService filterService;
