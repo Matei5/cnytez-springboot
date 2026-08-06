e8e0e66d833451e4674e5230e0f0ac9d3a6ef127
Matei5 <147913718+Matei5@users.noreply.github.com>
2026-08-06 11:22:30 +0300

    Added logs to docker logs, not only file

--- backend/src/main/java/cnytez/reddit/app/config/AppConfig.java
+++ backend/src/main/java/cnytez/reddit/app/config/AppConfig.java
@@ -4,7 +4,6 @@ import cnytez.reddit.app.log.ConsoleLogger;
 import cnytez.reddit.app.log.FileLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
