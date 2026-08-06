bf2e0a1227d4e3a3555b4d1f0e9643c33dc7b029
Andrei Preda <andreipreda998@gmail.com>
2026-08-06 13:41:20 +0300

    Update data.sql file

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,35 @@
-INSERT INTO filters (id, name, label) VALUES (0, 'none', 'No filter');
-INSERT INTO filters (id, name, label) VALUES (1, 'grayscale', 'Grayscale');
-INSERT INTO filters (id, name, label) VALUES (2, 'sepia', 'Sepia');
-INSERT INTO filters (id, name, label) VALUES (3, 'inverted', 'Inverted');
-INSERT INTO filters (id, name, label) VALUES (4, 'blur', 'Blur');
-INSERT INTO filters (id, name, label) VALUES (5, 'pixelated', 'Pixelated');
+INSERT INTO filters (id, name, label) VALUES (0, 'none', 'No filter')
+ON CONFLICT (id)
+DO UPDATE SET
+    name = EXCLUDED.name,
+    label = EXCLUDED.label;
+
+INSERT INTO filters (id, name, label) VALUES (1, 'grayscale', 'Grayscale')
+ON CONFLICT (id)
+DO UPDATE SET
+    name = EXCLUDED.name,
+    label = EXCLUDED.label;
+
+INSERT INTO filters (id, name, label) VALUES (2, 'sepia', 'Sepia')
+ON CONFLICT (id)
+DO UPDATE SET
+    name = EXCLUDED.name,
+    label = EXCLUDED.label;
+
+INSERT INTO filters (id, name, label) VALUES (3, 'inverted', 'Inverted')
+ON CONFLICT (id)
+DO UPDATE SET
+    name = EXCLUDED.name,
+    label = EXCLUDED.label;
+
+INSERT INTO filters (id, name, label) VALUES (4, 'blur', 'Blur')
+ON CONFLICT (id)
+DO UPDATE SET
+    name = EXCLUDED.name,
+    label = EXCLUDED.label;
+
+INSERT INTO filters (id, name, label) VALUES (5, 'pixelated', 'Pixelated')
+ON CONFLICT (id)
+DO UPDATE SET
+    name = EXCLUDED.name,
+    label = EXCLUDED.label;
