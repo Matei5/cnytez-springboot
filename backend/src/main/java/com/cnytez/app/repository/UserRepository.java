69da04ce39ed09983af371a5e708d5ed639c74c8
Stefan <stefan.cirlomaneanu@s.unibuc.ro>
2026-08-10 18:34:10 +0300

    Remove legacy user API

--- backend/src/main/java/com/cnytez/app/repository/UserRepository.java
+++ backend/src/main/java/com/cnytez/app/repository/UserRepository.java
@@ -4,16 +4,11 @@ import com.cnytez.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-
-    List<User> findAllByDeletionDateIsNull();
-
-    Optional<User> findByIdAndDeletionDateIsNull(UUID id);
 
     Optional<User> findByUsernameAndDeletionDateIsNull(String username);
 
