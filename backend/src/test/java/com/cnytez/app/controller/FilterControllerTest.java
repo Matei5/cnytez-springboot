49288940756fa02ec9764f5bd1d9b4c40d0be214
Matei5 <147913718+Matei5@users.noreply.github.com>
2026-08-17 01:43:31 +0300

    Implemented an abstract base for controller tests

--- backend/src/test/java/com/cnytez/app/controller/FilterControllerTest.java
+++ backend/src/test/java/com/cnytez/app/controller/FilterControllerTest.java
@@ -2,14 +2,9 @@ package com.cnytez.app.controller;
 
 import com.cnytez.app.dto.internal.FilterDto;
 import com.cnytez.app.service.FilterService;
-import com.cnytez.app.service.JwtService;
-import com.cnytez.app.logging.LogManager;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
@@ -18,17 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(FilterController.class)
-@AutoConfigureMockMvc(addFilters = false)
-class FilterControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockitoBean
-    private JwtService jwtService;
-    
-    @MockitoBean
-    private LogManager logManager;
+class FilterControllerTest extends BaseControllerTest {
 
     @MockitoBean
     private FilterService filterService;
@@ -37,7 +22,7 @@ class FilterControllerTest {
     void getFilters_success() throws Exception {
         FilterDto filter1 = new FilterDto(1, "new", "New");
         FilterDto filter2 = new FilterDto(2, "top", "Top");
-        
+
         when(filterService.getAllFilters()).thenReturn(List.of(filter1, filter2));
 
         mockMvc.perform(get("/filters"))
